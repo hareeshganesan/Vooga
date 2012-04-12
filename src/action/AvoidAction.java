773e7d2e50e34c4d0e9388e1ad56e51b6835f5d6
Peggy Li <peggyli.224@gmail.com>
2012-04-12 18:09:28 -0400

    Revert to earlier commit (before pushing wrong files)

--- src/action/AvoidAction.java
+++ src/action/AvoidAction.java
@@ -3,9 +3,11 @@ package action;
 import java.awt.geom.Point2D;
 import sprite.FighterSprite;
 
+
 public class AvoidAction extends Action
 {
     FighterSprite myEnemy;
+
 
     public AvoidAction (FighterSprite fighter, FighterSprite enemy)
     {
@@ -13,16 +15,22 @@ public class AvoidAction extends Action
         myEnemy = enemy;
     }
 
+
     @Override
     public void performAction (long elapsedTime)
     {
         double dist =
-                myFighter.getCurrentLocation().distance(myEnemy.getCurrentLocation());
-            double x_direction = -(myEnemy.getX() - myFighter.getX()) / dist;
-            double y_direction = -(myEnemy.getY() - myFighter.getY()) / dist;
-        MotionAction f = new MotionAction(myFighter, new Point2D.Double(x_direction, y_direction));
-        f.performAction(elapsedTime);
-        
+            myFighter.getCurrentLocation()
+                     .distance(myEnemy.getCurrentLocation());
+        double x_direction = -(myEnemy.getX() - myFighter.getX()) / dist;
+        double y_direction = -(myEnemy.getY() - myFighter.getY()) / dist;
+        if (dist < 200)
+        {
+            MotionAction f =
+                new MotionAction(myFighter, new Point2D.Double(x_direction,
+                                                               y_direction));
+            f.performAction(elapsedTime);
+        }
     }
 
 }
