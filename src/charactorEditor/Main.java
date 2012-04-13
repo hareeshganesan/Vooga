008f49e4d5cab4d672f02dce68c26103191649f6
chen <Chen@Chen-THINK.(none)>
2012-04-13 01:17:33 -0400

    charactorEditor

---
+++ src/charactorEditor/Main.java
@@ -0,0 +1,11 @@
+package charactorEditor;
+import java.io.FileNotFoundException;
+
+
+import charactorEditor.drag.FighterBuilder;
+
+public class Main{
+	public static void main(String[] args) throws FileNotFoundException{
+		new FighterBuilder();
+	}
+}
---
+++ src/charactorEditor/Writer.java
@@ -0,0 +1,22 @@
+package charactorEditor;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+public class Writer {
+
+	public static void write(Object toWrite, String file) throws IOException {
+       if(!file.endsWith(".json"))
+    	   file=file+".json";
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(toWrite);
+		FileWriter fileOut = new FileWriter(file);
+		BufferedWriter out = new BufferedWriter(fileOut);
+		out.write(jsonString);
+		out.close();
+	}
+}
